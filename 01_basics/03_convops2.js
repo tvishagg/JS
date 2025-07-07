e9447d818772680a0393519a30b75f64f7d6c6b4
anishka22m <anishka22m@gmail.com>
2025-07-07 19:42:20 +0530

    conv ops2

---
+++ 01_basics/03_convops2.js
@@ -0,0 +1,22 @@
+/* let isLoggedIn = 1 
+
+let bool_loggedIn = Boolean(isLoggedIn)
+
+
+console.log(bool_loggedIn) */ //output true 
+
+/* let isLoggedIn = ""
+
+let bool_loggedIn = Boolean(isLoggedIn)
+
+
+console.log(bool_loggedIn) */  //output false  
+
+/* let isLoggedIn = "ani"
+
+let bool_loggedIn = Boolean(isLoggedIn)
+
+
+console.log(bool_loggedIn)*/ //output true 
+
+
